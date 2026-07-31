e4f3328c2edce64a747317e06740d567956ae989
JustJabka <ivanmartinisin49@gmail.com>
2026-07-31 19:51:03 +0300

    Added environment aura sub-command for /sanity. Tweaked command translates. Merged sanity manipulation in the SanityCommand to one method

--- src/main/java/justjabka/project_sanguine/contents/command/SanityCommand.java
+++ src/main/java/justjabka/project_sanguine/contents/command/SanityCommand.java
@@ -3,62 +3,59 @@ package justjabka.project_sanguine.contents.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import justjabka.project_sanguine.contents.attachment.PlayerData;
+import justjabka.project_sanguine.managers.SanityManager;
 import justjabka.project_sanguine.registries.ProjectSanguineAttachments;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import org.apache.commons.lang3.function.TriFunction;
 
 public class SanityCommand {
+    private enum Operation {
+        ADD("add", PlayerData::addSanity),
+        REMOVE("remove", PlayerData::removeSanity),
+        SET("set", PlayerData::setSanity);
+
+        private final String key;
+        private final TriFunction<PlayerData, ServerPlayer, Float, PlayerData> function;
+
+        Operation(String key, TriFunction<PlayerData, ServerPlayer, Float, PlayerData> function) {
+            this.key = key;
+            this.function = function;
+        }
+    }
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("sanity")
+        LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("sanity")
                 .requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
-                .then(Commands.literal("set")
-                        .then(Commands.argument("target", EntityArgument.player())
-                                .then(Commands.argument("value", FloatArgumentType.floatArg(0))
-                                        .executes(SanityCommand::setSanity)
-                                )
-                        )
-                )
                 .then(Commands.literal("get")
                         .then(Commands.argument("target", EntityArgument.player())
                                 .executes(SanityCommand::getSanity)
-                        )
-                )
-                .then(Commands.literal("add")
-                        .then(Commands.argument("target", EntityArgument.player())
-                                .then(Commands.argument("value", FloatArgumentType.floatArg(0))
-                                        .executes(SanityCommand::addSanity)
+                                .then(Commands.literal("environment")
+                                        .executes(SanityCommand::getSanityAura)
                                 )
                         )
-                )
-                .then(Commands.literal("remove")
-                        .then(Commands.argument("target", EntityArgument.player())
-                                .then(Commands.argument("value", FloatArgumentType.floatArg(0))
-                                        .executes(SanityCommand::removeSanity)
-                                )
-                        )
-                )
-        );
-    }
+                );
 
-    private static int setSanity(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        ServerPlayer player = EntityArgument.getPlayer(context, "target");
-        float value = FloatArgumentType.getFloat(context, "value");
+        for (Operation operation : Operation.values()) {
+            builder.then(Commands.literal(operation.key)
+                    .then(Commands.argument("target", EntityArgument.player())
+                            .then(Commands.argument("value", FloatArgumentType.floatArg(0))
+                                    .executes(context ->
+                                            manipulateSanity(context, operation)
+                                    )
+                            )
+                    )
+            );
+        }
 
-        PlayerData data = player.getAttachedOrCreate(ProjectSanguineAttachments.PLAYER_DATA);
-        player.setAttached(
-                ProjectSanguineAttachments.PLAYER_DATA,
-                data.setSanity(player, value)
-        );
-
-        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.set.success.single", value, player.getDisplayName()), true);
-
-        return Command.SINGLE_SUCCESS;
+        dispatcher.register(builder);
     }
 
     private static int getSanity(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
@@ -66,37 +63,40 @@ public class SanityCommand {
 
         PlayerData data = player.getAttachedOrCreate(ProjectSanguineAttachments.PLAYER_DATA);
 
-        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.get.success.single", player.getDisplayName(), data.sanity()), true);
+        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.get.success.single",
+                player.getDisplayName(),
+                data.sanity()
+        ), true);
 
         return Command.SINGLE_SUCCESS;
     }
 
-    private static int addSanity(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+    private static int getSanityAura(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        ServerPlayer player = EntityArgument.getPlayer(context, "target");
+
+        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.get.environment.success.single",
+                player.getDisplayName(),
+                SanityManager.getPassiveAura(player)
+        ), true);
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int manipulateSanity(CommandContext<CommandSourceStack> context, Operation operation) throws CommandSyntaxException {
         ServerPlayer player = EntityArgument.getPlayer(context, "target");
         float value = FloatArgumentType.getFloat(context, "value");
 
         PlayerData data = player.getAttachedOrCreate(ProjectSanguineAttachments.PLAYER_DATA);
         player.setAttached(
                 ProjectSanguineAttachments.PLAYER_DATA,
-                data.addSanity(player, value)
+                operation.function.apply(data, player, value)
         );
 
-        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.add.success.single", value, player.getDisplayName()), true);
-
-        return Command.SINGLE_SUCCESS;
-    }
-
-    private static int removeSanity(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        ServerPlayer player = EntityArgument.getPlayer(context, "target");
-        float value = FloatArgumentType.getFloat(context, "value");
-
-        PlayerData data = player.getAttachedOrCreate(ProjectSanguineAttachments.PLAYER_DATA);
-        player.setAttached(
-                ProjectSanguineAttachments.PLAYER_DATA,
-                data.removeSanity(player, value)
-        );
-
-        context.getSource().sendSuccess(() -> Component.translatable("commands.sanity.remove.success.single", value, player.getDisplayName()), true);
+        String translationKey = "commands.sanity.%s.success.single".formatted(operation.key);
+        context.getSource().sendSuccess(() -> Component.translatable(translationKey,
+                value,
+                player.getDisplayName()
+        ), true);
 
         return Command.SINGLE_SUCCESS;
     }
